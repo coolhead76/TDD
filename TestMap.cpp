a807e14e07e744570178a7561537d1006261acff
coolhead76 <31235368+coolhead76@users.noreply.github.com>
2020-03-26 23:55:40 +0800

    add TestMap.cpp

---
+++ TestMap.cpp
@@ -0,0 +1,34 @@
+#include "TestMap.h"
+#include <map>
+#include <iostream>
+using namespace std;
+
+
+TestMap::TestMap()
+{
+	map<char, int> amap;
+	
+	
+}
+
+TestMap::~TestMap()
+{
+	
+
+	cout << "this is" << endl;
+	cout << "this is" << endl;
+	cout << "this is" << endl;
+
+
+}
+
+TestMap::test()
+{
+
+}
+ 
+TestMap::test2()
+{
+
+	
+}
