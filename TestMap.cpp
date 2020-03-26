465d661c68216f432782d75272c1d0018564fa92
coolhead76 <31235368+coolhead76@users.noreply.github.com>
2020-03-27 00:05:12 +0800

    commit from roll, branch 2

---
+++ TestMap.cpp
@@ -0,0 +1,35 @@
+#include "TestMap.h"
+#include <map>
+#include <iostream>
+using namespace std;
+
+
+TestMap::TestMap()
+{
+	map<char, int> amap;
+	
+	
+}
+
+TestMap::~TestMap()
+{
+	
+	this is from branch 2;
+	cout << "this is" << endl;
+	cout <<endl;
+	added in branch 2;
+	cout << "this is" << endl;
+
+
+}
+
+TestMap::test()
+{
+
+}
+ 
+TestMap::test2()
+{
+
+	
+}
